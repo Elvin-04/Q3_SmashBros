7e08e20b1bd2b79dfb84e55cf06c3a5e0c34b752
Geoffrey Laurent <114664596+Albators974@users.noreply.github.com>
2023-11-10 16:02:29 +0100

    Multiplayer + attack
    whe can use 1, 2, 3 or 4 controller to play this game and we have a base attack , up attack, side attack, down attack

---
+++ Q3_LaHordeSmashdown/Assets/PlayerManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager instance;
+
+    public List<GameObject> _playerList;
+    public GameObject _map;
+    public List<GameObject> _mapChilder;
+
+    void Start()
+    {
+        instance = this;
+        _playerList = new List<GameObject>();
+        _mapChilder = new List<GameObject>();
+        for (int i = 0; i < _map.transform.childCount; i++)
+        {
+            _mapChilder.Add(_map.transform.GetChild(i).gameObject);
+        }
+    }
+}
