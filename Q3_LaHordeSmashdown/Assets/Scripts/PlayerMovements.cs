057778f72e5ad126cba9f1d14e9170ffb152e7b7
Elvin-04 <e.berger@lahorde.school>
2023-11-12 20:16:34 +0100

    Double jump

--- Q3_LaHordeSmashdown/Assets/Scripts/PlayerMovements.cs
+++ Q3_LaHordeSmashdown/Assets/Scripts/PlayerMovements.cs
@@ -1,54 +1,50 @@
-using System.Globalization;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Tilemaps;
 
 public class PlayerMovements : MonoBehaviour
 {
 
     public float speed = 5f;
     public float jumpForce = 15f;
+    public LayerMask groundLayer;
 
     Rigidbody2D rb;
     Transform myTransform;
 
     private Vector2 leftJoystickValue;
-    private int jumpCount = 0;
+    private int jumpCount = 2;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         myTransform = GetComponent<Transform>();
+
+        jumpCount = 2;
     }
 
-    
+
+
 
     private void Update()
     {
-        if(leftJoystickValue.x < -0.5f)
-        {
+        if(leftJoystickValue.x < -0.5f) { 
             myTransform.Translate(new Vector2(-speed * Time.deltaTime, 0));
         }
-        else if(leftJoystickValue.x > 0.5f)
-        {
+        else if(leftJoystickValue.x > 0.5f) {
             myTransform.Translate(new Vector2(speed * Time.deltaTime, 0));
         }
     }
 
     private void Jump()
     {
-        rb.velocity = Vector2.zero;
-        rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-        jumpCount++;
+        if(jumpCount > 0)
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jumpCount--;
+        }
+        
     }
-
-    private bool IsGrounded()
-    {
-
-
-        return false;
-    }
-
 
 
     public void OnMove(InputAction.CallbackContext context)
@@ -58,9 +54,17 @@ public class PlayerMovements : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if(context.performed && jumpCount <= 1)
+        if(context.performed)
         {
             Jump();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == groundLayer)
+        {
+            jumpCount = 2;
         }
     }
 }
