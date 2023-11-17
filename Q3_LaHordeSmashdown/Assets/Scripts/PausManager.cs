c7b9d4a342eb5104bb071ab256057e8f516d5e01
Geoffrey Laurent <114664596+Albators974@users.noreply.github.com>
2023-11-17 11:34:37 +0100

    map and menu fix

--- Q3_LaHordeSmashdown/Assets/Scripts/PausManager.cs
+++ Q3_LaHordeSmashdown/Assets/Scripts/PausManager.cs
@@ -16,6 +16,7 @@ public class PausManager : MonoBehaviour
         instance = this;
         _paused = false;
         _pausCanvas.gameObject.SetActive(false);
+        _pausAnimator.SetBool("UnPaus", false);
     }
 
     public void PausResumaGame()
@@ -31,25 +32,29 @@ public class PausManager : MonoBehaviour
         {
             Time.timeScale = 0;
             _pausCanvas.gameObject.SetActive(_paused);
-            _pausAnimator.Play("PausMenu");
+            _pausAnimator.SetBool("UnPaus", false);
         }
         else if (_paused == false)
         {
+            _pausAnimator.SetBool("UnPaus", true);
             StartCoroutine(WaitToUnableCanvasPausMenu());
         }
     }
 
     IEnumerator WaitToUnableCanvasPausMenu()
     {
-        _pausAnimator.Play("UnPausMenu");
         Time.timeScale = 1;
         yield return new WaitForSeconds(1f);
         _pausCanvas.gameObject.SetActive(_paused);
+        _pausAnimator.SetBool("UnPaus", false);
     }
     
     public void MainMenu(string mainMenuSceneName)
     {
-        Time.timeScale = 1;
-        SceneManager.LoadScene(mainMenuSceneName);
+        if (_paused)
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(mainMenuSceneName);
+        }
     }
 }
